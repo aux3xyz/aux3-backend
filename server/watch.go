b60cedee8f739dca4ade7626bca0a41bcfb43b61
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-11-01 01:04:56 +0530

    fix: function return type

--- server/watch.go
+++ server/watch.go
@@ -1,54 +1,56 @@
 package server
 
 import (
-    "context"
-    "log"
-    "time"
+	"context"
+	"log"
+	"time"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/bson"
-
-    "aux3.xyz/common"
+	"aux3.xyz/common"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func Watch() {
-    // Get the shared MongoDB client
-    client := common.GetMongoClient()
-    ctx := context.Background()
+func Watch() error {
+	// Get the shared MongoDB client
+	client := common.GetMongoClient()
+	ctx := context.Background()
 
-    // Get collection to watch
-    // TODO: setup right database and collection names
-    collection := client.Database("aux3").Collection("events")
+	// Get collection to watch
+	// TODO: setup right database and collection names
+	collection := client.Database("aux3").Collection("events")
 
-    // Set up change stream
-    pipeline := mongo.Pipeline{}
-    opts := options.ChangeStream().SetFullDocument(options.UpdateLookup)
-    
-    // Start watching for changes
-    changeStream, err := collection.Watch(ctx, pipeline, opts)
-    if err != nil {
-        log.Fatalf("Failed to start change stream: %v", err)
-    }
-    defer changeStream.Close(ctx)
+	// Set up change stream
+	pipeline := mongo.Pipeline{}
+	opts := options.ChangeStream().SetFullDocument(options.UpdateLookup)
 
-    // Loop forever watching for changes
-    for changeStream.Next(ctx) {
-        var event bson.M
-        if err := changeStream.Decode(&event); err != nil {
-            log.Printf("Error decoding change event: %v", err)
-            continue
-        }
+	// Start watching for changes
+	changeStream, err := collection.Watch(ctx, pipeline, opts)
+	if err != nil {
+		log.Printf("Failed to start change stream: %v", err)
+		return err
+	}
+	defer changeStream.Close(ctx)
 
-        // Send event to process
-        // TODO: Setup Process function
-        go Process()
+	// Loop forever watching for changes
+	for changeStream.Next(ctx) {
+		var event bson.M
+		if err := changeStream.Decode(&event); err != nil {
+			log.Printf("Error decoding change event: %v", err)
+			continue
+		}
 
-        // Basic error handling with reconnection
-        if err := changeStream.Err(); err != nil {
-            log.Printf("Error in change stream: %v", err)
-            time.Sleep(1 * time.Second)
-            return Watch() // Recursive call to restart watching
-        }
-    }
+		// Send event to process
+		// TODO: Setup Process function
+		go Process()
+
+		// Error handling with reconnection
+		if err := changeStream.Err(); err != nil {
+			log.Printf("Error in change stream: %v", err)
+			time.Sleep(1 * time.Second)
+			return Watch() // Recursive call to restart watching
+		}
+	}
+
+	return nil
 }
